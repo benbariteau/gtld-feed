c3b872701f80ab4d0a233104cfe6f9f79741119a
Ben Bariteau <ben.bariteau@gmail.com>
2018-08-15 23:40:19 -0700

    get new tlds and parse response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate reqwest;
+
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let new_tlds_text = reqwest::get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt").unwrap().text().unwrap();
+
+    // split on newline and then skip the first line, which is always the generated time comment
+    let new_tlds: HashSet<&str> = new_tlds_text.split("\n").skip(1).map(|tld| tld.trim()).collect();
+
+    for tld in new_tlds {
+        println!("{}", tld);
+    }
 }
